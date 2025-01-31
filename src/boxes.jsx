c1847b877093f9c44695555b88440300582950f7
Aryan Shrivastava <asguy29@gmail.com>
2025-02-01 00:09:12 +0530

    ui and func changes

--- src/boxes.jsx
+++ src/boxes.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 export const Playboxes=()=>{
 
+
     const winPatterns=[
         [0,1,2],
         [0,3,6],
@@ -13,23 +14,23 @@ export const Playboxes=()=>{
         [6,7,8],
     ];
 
-    const[popUp,setPopUp]=useState("")
+    const [popUp, setPopUp] = useState(null);
     const [turn0, setTurn0] = useState(true); 
-    const[disable,setDisabled]=useState(false);
+    const [disable, setDisabled] = useState(false);
 
-
-    const toggleX=()=>{
-        setPopUp(turn0 ? "X" : "O");
-        setDisabled(true);
-        setTurn0(!turn0);
-    }
+const toggleX = () => {
+    setPopUp(turn0 ? "X" : "O");
+    setTurn0(!turn0);
+    setDisabled(true);
+};
 
 
 
     return(
         <>
-        <div className="border-2 border-red-200 text-blue-100 h-35 w-35 flex 
-                items-center justify-center text-9xl rounded-md cursor-pointer caret-transparent" onClick={!disable ? ()=>{toggleX(); disable();}:"error"}>{popUp}</div>
+        <div className="border-2 border-blue-200 text-blue-100 h-35 w-35
+                 text-9xl rounded-xl cursor-pointer caret-transparent 
+                 text-center transition-transform duration-400 hover:scale-110 active:scale-110" onClick={!disable ? ()=>{toggleX(); disable();}:"error"}>{popUp}</div>
         </>
     )
 }
--- src/mainpage.jsx
+++ src/mainpage.jsx
@@ -1,13 +1,19 @@
 import{Playboxes} from "./boxes";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const Mainpage=()=>{
 
+    const[reset,setReset]=useState(null);
+
+    const resetGame=()=>{
+        setReset("")
+    }
+
     return(
         <>
-        <div className="h-screen bg-gradient-to-r from-slate-900 to-slate-700">
-        <div className=" flex items-center justify-center h-auto w-auto relative">
-            <div className="grid grid-cols-3 gap-2 h-auto w-auto ">
+        <div className="h-screen w-screen bg-gradient-to-r from-slate-900 to-slate-700 fixed">
+        <div className=" flex items-center justify-center  p-2 my-10">
+            <div className="grid grid-cols-3 gap-4 h-auto w-auto ">
                 <Playboxes/>
                 <Playboxes/>
                 <Playboxes/>
@@ -20,7 +26,8 @@ export const Mainpage=()=>{
             </div>
             
          </div>
-        
+        <button className="mx-auto flex my-10 text-black font-bold font-sans bg-blue-300 p-2 border-rounded cursor-pointer 
+         caret-transparent rounded-xl transition-transform duration-400 hover:scale-110">Reset Game</button>
          </div>
          
         </>
