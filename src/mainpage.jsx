a443bbb1abd2eeb46ab2249f04bfc6b5a60f07e5
Aryan Shrivastava <asguy29@gmail.com>
2025-02-06 14:52:40 +0530

    responsive site

--- src/mainpage.jsx
+++ src/mainpage.jsx
@@ -7,22 +7,21 @@ function TicTacToe() {
     <div className="min-h-screen w-screen bg-gradient-to-r from-slate-900 to-slate-700 fixed">
     <div className="mx-auto text-center text-blue-200 h-auto w-auto p-2">
       <div>
-        <div className="text-3xl p-2 my-2 font-bold font-sans">
+        <div className="text-xl p-2 my-2 font-bold font-sans sm:text-2xl ">
         {getStatusMessage()}
         </div>
-        <button className="p-2 font-bold bg-blue-500 rounded-2xl text-blue-100 cursor-pointer" onClick={resetGame}>
+        <button className="p-2 font-bold text-balance bg-blue-500 rounded-xl text-blue-100 cursor-pointer" onClick={resetGame}>
           Reset Game
         </button>
       </div>
 
-      <div className="grid grid-cols-3 gap-4 p-4 w-fit mx-auto my-3">
+      <div className="grid grid-cols-3 gap-3 p-4 w-fit mx-auto my-3 sm:gap-4">
         {board.map((b, index) =>{
           return (
             <button
-              className="border-2 border-blue-200 text-blue-100 h-32 w-32
-              text-8xl rounded-xl cursor-pointer caret-transparent 
+              className="border-2 border-blue-200 text-blue-100 h-16 w-16 text-4xl rounded-xl cursor-pointer caret-transparent 
               text-center flex items-center justify-center 
-              transition-transform duration-400 hover:scale-110 active:scale-110"
+              transition-transform duration-400 hover:scale-110 active:scale-110 sm:h-32 sm:w-32 sm:text-8xl"
               
               key={index}
               onClick={() => handleClick(index)}
